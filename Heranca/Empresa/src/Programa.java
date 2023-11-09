8e54c450a366e04177824765784e6dc91a82baf6
Weslley Oliveira <145867513+weslleydasilvaoliveira@users.noreply.github.com>
2023-11-09 06:55:10 -0300

    Update Programa.java

--- Heranca/Empresa/src/Programa.java
+++ Heranca/Empresa/src/Programa.java
@@ -5,7 +5,7 @@ public class Programa {
 		Locale.setDefault(Locale.US);
 		
 		Funcionario fNormal = new Funcionario("Wellington Silva",10, 100);
-		Terceirizado terceirizado = new Terceirizado("John Constatine", 10, 100,1.1);
+		Terceirizado terceirizado = new Terceirizado("John Constantine", 10, 100,1.1);
 		
 		System.out.println("FUNCIONARIOS:");
 		System.out.println(fNormal.getNome());
